aa26c4e0c5ea33ded03733ffb8867e47bbfa3114
Zachary Landes <zach.g.landes@gmail.com>
2018-09-25 16:49:28 -0400

    adding address and phone attributes

--- app/models/employer_events/employer_importer.rb
+++ app/models/employer_events/employer_importer.rb
@@ -1,6 +1,7 @@
 module EmployerEvents
   class EmployerImporter
     XML_NS = { "cv" => "http://openhbx.org/api/terms/1.0" }
+    include HappyMapper
 
     attr_reader :xml
 
@@ -21,12 +22,59 @@ module EmployerEvents
       company_name = stripped_node_value(company_name_node)
       dba = stripped_node_value(dba_node)
       fein = stripped_node_value(fein_node)
+      
       {
         hbx_id: hbx_id,
         fein: fein,
         dba: dba,
-        name: company_name
+        name: company_name,
       }
+    end
+    
+    def demographic_values 
+
+      type = @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:address//cv:type", XML_NS).first
+      address_line_1 =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:address//cv:address_line_1", XML_NS).first
+      location_city_name =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:address//cv:location_city_name", XML_NS).first
+      location_state_code =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:address//cv:location_state_code", XML_NS).first
+      postal_code =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:address//cv:postal_code", XML_NS).first
+      phone_type =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:phone//cv:type", XML_NS).first
+      phone_number =  @xml.xpath("//cv:organization/cv:office_locations//cv:office_location//cv:phone//cv:full_phone_number", XML_NS).first
+
+      address_type = stripped_node_value(type).split("#").last
+      address_line_1 = stripped_node_value(address_line_1)
+      city = stripped_node_value(location_city_name)
+      state = stripped_node_value(location_state_code)
+      zip = stripped_node_value(postal_code)
+      phone_type = stripped_node_value(phone_type).split("#").last
+      phone_number = stripped_node_value(phone_number)
+
+      {
+        address: {
+            address_type: address_type,
+            address_1: address_line_1,
+            city:city ,
+            location_state_code: state,
+            zip: zip
+        },
+
+        phone: {
+          phone_type: phone_type, 
+          full_phone_number: phone_number
+
+        }
+      }
+    end
+
+    def add_employer_demographics(demographic_values, employer)
+      employer.addresses << Address.new(demographic_values[:address])
+      employer.phones << Phone.new(demographic_values[:phone])
+      employer.save
+    end
+
+    def update_employer_demographics(demographic_values, employer)  
+      employer.addresses.first.update_attributes!(demographic_values[:address])
+      employer.phones.first.update_attributes!(demographic_values[:phone])
     end
 
     def plan_year_values
@@ -45,12 +93,17 @@ module EmployerEvents
     def persist
       return unless importable?
       existing_employer = Employer.where({:hbx_id => employer_values[:hbx_id]}).first
+      
       employer_record = if existing_employer
                           existing_employer.update_attributes!(employer_values)
+                          update_employer_demographics(demographic_values, existing_employer)
                           existing_employer
                         else
-                          Employer.create!(employer_values)
+                          employer = Employer.create!(employer_values)
+                          add_employer_demographics(demographic_values, employer)
+                          employer
                         end
+                        
       employer_id = employer_record.id
       existing_plan_years = employer_record.plan_years
       plan_year_values.each do |pyvs|

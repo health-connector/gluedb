046c143c43572b4220d196c74e92f22df273b9e3
Zachary Landes <zach.g.landes@gmail.com>
2018-10-18 16:08:25 -0400

    updating to find member by hbx id

--- app/data_migrations/add_dependent.rb
+++ app/data_migrations/add_dependent.rb
@@ -6,7 +6,7 @@ class AddDependent < MongoidMigrationTask
 
   def migrate
     policy = Policy.where(eg_id: ENV['eg_id']).first
-    person = Person.where(authority_member_id: ENV['hbx_id']).first
+    person = Person.where('members.hbx_member_id' => ENV['hbx_id']).first
     if policy.blank?
         puts "Could not find a matching policy" unless Rails.env.test?
     elsif person.blank?
@@ -20,9 +20,9 @@ class AddDependent < MongoidMigrationTask
     if is_already_member?(policy, person)
       puts "This person is already an enrollee on this policy" unless Rails.env.test?
     else 
-      enrollee = policy.enrollees.create!(m_id:person.authority_member_id, rel_code:ENV['rel_code'])
-      enrollee.update_attributes!(coverage_start: ENV["coverage_start"])
-      enrollee.update_attributes!(coverage_end: ENV["coverage_end"])
+      enrollee = policy.enrollees.create!(m_id: ENV['hbx_id'], rel_code:ENV['rel_code'])
+      enrollee.update_attributes!(coverage_start: ENV["coverage_start"]) if  ENV["coverage_start"]
+      enrollee.update_attributes!(coverage_end: ENV["coverage_end"]) if  ENV["coverage_end"]  
       puts "Successfully updated enrollee: #{enrollee.m_id} and policy: #{policy.eg_id}" unless Rails.env.test?
     end
   end

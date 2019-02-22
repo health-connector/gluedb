e9a71c6ef98b0fbb1fc14d87e0ae66440cda0a68
Zachary Landes <zach.g.landes@gmail.com>
2019-02-22 14:36:44 -0500

    REFS 40016 - updating carrier ids to mongo and building py loop method
    testing employer event
    
    updating plan year model to have issuer profile ids
    
    adding has and belongs to many association with carriers
    
    adding 40013 functionality
    
    adding issuer ids to the plan year create method
    
    pulling in py update
    
    merging feature_40013
    
    udpating event types based on previous plan year
    
    updating carrier ids to mongo, catching nil errors in renderer
    
    updating py loop

--- app/models/employer_events/employer_importer.rb
+++ app/models/employer_events/employer_importer.rb
@@ -42,6 +42,46 @@ module EmployerEvents
       end
     end
 
+    def plan_year_loop(pyvs)
+      start_date = pyvs[:start_date].strftime("%Y%m%d")
+      end_date = pyvs[:end_date].strftime("%Y%m%d")
+      @xml.xpath("//cv:plan_year", XML_NS).select do |node|
+        stripped_node_value(node.xpath("cv:plan_year_start", XML_NS).first) == start_date && 
+        stripped_node_value(node.xpath("cv:plan_year_end", XML_NS).first) == end_date
+      end
+    end
+
+    def issuer_ids(pyvs)
+      plan_year_loop(pyvs).map do |outer_node|
+        outer_node.xpath("cv:benefit_groups", XML_NS).map do |node|
+          node.xpath("cv:benefit_group/cv:elected_plans", XML_NS).map do |inner_node|
+            ids = inner_node.xpath("cv:elected_plan/cv:carrier/cv:id/cv:id", XML_NS).map do |id|
+              stripped_node_value(id)
+            end
+          return ids.flatten || nil
+          end
+        end
+      end
+    end
+
+    def carrier_mongo_ids(pyvs)
+      issuer_ids(pyvs).flatten.map do |hbx_carrier_id|
+          Carrier.where(hbx_carrier_id: hbx_carrier_id).first.id
+        end
+    end
+
+    def create_plan_year(pyvs, employer_id)
+      pyvs.merge!(:employer_id => employer_id)
+      pyvs.merge!(:issuer_ids => carrier_mongo_ids(pyvs)) if carrier_mongo_ids(pyvs).present?
+      PlanYear.create!(pyvs)
+    end
+
+    def update_plan_years(pyvs, employer)
+      plan_year = employer.plan_years.detect{|py|py.start_date == pyvs[:start_date] && py.end_date == pyvs[:end_date] } 
+      plan_year.update_attributes!(:issuer_ids => carrier_mongo_ids(pyvs)) if carrier_mongo_ids(pyvs).present?
+    end
+
+
     def persist
       return unless importable?
       existing_employer = Employer.where({:hbx_id => employer_values[:hbx_id]}).first
@@ -61,8 +101,10 @@ module EmployerEvents
           epy_end = epy.end_date ? epy.end_date : (epy.start_date + 1.year - 1.day)
           (epy_start..epy_end).overlaps?((start_date..end_date))
         end 
-        if !matching_plan_years
-          PlanYear.create!(pyvs.merge(:employer_id => employer_id))
+        if matching_plan_years
+          update_plan_years(pyvs, employer_record)
+        else
+          create_plan_year(pyvs, employer_id)
         end
       end
     end

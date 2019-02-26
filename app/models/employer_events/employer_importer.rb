d7bbf4a82927ba947b89f2fc76df9ab4b0742a64
Zachary Landes <zach.g.landes@gmail.com>
2019-02-26 15:47:58 -0500

    adding 40013 functionality

--- app/models/employer_events/employer_importer.rb
+++ app/models/employer_events/employer_importer.rb
@@ -42,6 +42,23 @@ module EmployerEvents
       end
     end
 
+    def issuer_ids 
+      @xml.xpath("//cv:organization/cv:employer_profile/cv:benefit_groups", XML_NS).map do |node|
+        ids = node.xpath("cv:benefit_group/cv:carrier/cv:id/cv:id", XML_NS).map do |id|
+        stripped_node_value(id)
+       end
+       return ids.flatten || nil
+      end
+    end
+
+    def create_plan_year(pyvs, employer_id)
+      if issuer_ids.present?
+        PlanYear.create!(pyvs.merge(:employer_id => employer_id, issuer_ids: issuer_ids))
+      else
+        PlanYear.create!(pyvs.merge(:employer_id => employer_id))
+      end
+    end
+
     def persist
       return unless importable?
       existing_employer = Employer.where({:hbx_id => employer_values[:hbx_id]}).first
@@ -62,7 +79,7 @@ module EmployerEvents
           (epy_start..epy_end).overlaps?((start_date..end_date))
         end 
         if !matching_plan_years
-          PlanYear.create!(pyvs.merge(:employer_id => employer_id))
+          create_plan_year(pyvs, employer_id)
         end
       end
     end

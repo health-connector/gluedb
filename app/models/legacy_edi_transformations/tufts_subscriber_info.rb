88c5566e55ed084b5bcceb9c4aeae26a7900e117
Trey Evans <tevans@Just-Plain-Awful.local>
2019-04-23 17:11:45 -0400

    Move to usage of 1L now that it is provided.

--- app/models/legacy_edi_transformations/tufts_subscriber_info.rb
+++ app/models/legacy_edi_transformations/tufts_subscriber_info.rb
@@ -2,38 +2,24 @@ module LegacyEdiTransformations
   class TuftsSubscriberInfo
     attr_reader :subscriber_id,
                 :hios_id,
-                :carrier_assigned_policy_id,
+                :exchange_assigned_policy_id,
                 :subscriber_start
-    def initialize(sub_id, h_id, capi, s_start)
+    def initialize(sub_id, h_id, eapi, s_start)
       @subscriber_id = sub_id
       @hios_id = h_id
-      @carrier_assigned_policy_id = capi
+      @exchange_assigned_policy_id = eapi
       @subscriber_start = s_start
     end
 
     def locate_policy_information
-      return nil if subscriber_id.blank?
-      return nil if hios_id.blank?
-      return nil if subscriber_start.blank?
-      plan_ids = Plan.where(:hios_plan_id => hios_id).map(&:id)
-      return nil if plan_ids.blank?
+      return nil if @exchange_assigned_policy_id.blank?
       potential_policies = Policy.where(
         {
-          :plan_id => {"$in" => plan_ids},
-          "enrollees.m_id" => subscriber_id
+          :eg_id => exchange_assigned_policy_id
         }
       )
       return nil if potential_policies.blank?
-      matching_pols = potential_policies.select do |pol|
-        pol.enrollees.any? do |en|
-          en.subscriber? &&
-            (en.m_id == subscriber_id) &&
-            en.coverage_start.present? &&
-            (subscriber_start == en.coverage_start.strftime("%Y%m%d"))
-        end
-      end
-      return nil if matching_pols.blank?
-      extract_needed_information(matching_pols.first)
+      extract_needed_information(potential_policies.first)
     end
 
     protected

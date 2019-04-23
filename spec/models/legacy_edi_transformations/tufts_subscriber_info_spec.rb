2cd2a261d33224913814f4b9b6f36120d2275e4b
Trey Evans <tevans@Just-Plain-Awful.local>
2019-04-23 17:11:45 -0400

    Move to usage of 1L now that it is provided.

--- spec/models/legacy_edi_transformations/tufts_subscriber_info_spec.rb
+++ spec/models/legacy_edi_transformations/tufts_subscriber_info_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
   - given a blank subscriber id
-  - given a carrier assigned policy id
+  - given a blank exchange assigned policy id
   - given a hios id
   - given a subscriber start
+  - the policy does not exist
 " do
   let(:subscriber_id) { nil }
-  let(:capi) { "A CARRIER ASSIGNED POLICY ID" }
+  let(:eapi) { nil }
   let(:hios_id) { "A HIOS ID" }
   let(:subscriber_start) { "20180301" }
 
@@ -15,7 +16,7 @@ describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
     LegacyEdiTransformations::TuftsSubscriberInfo.new(
       subscriber_id,
       hios_id,
-      capi,
+      eapi,
       subscriber_start
     )
   end
@@ -26,64 +27,14 @@ describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
 end
 
 describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
-  - given a subscriber id
-  - given a carrier assigned policy id
-  - given a blank hios id
-  - given a subscriber start
-" do
-  let(:subscriber_id) { "A SUBSCRIBER ID" }
-  let(:capi) { "A CARRIER ASSIGNED POLICY ID" }
-  let(:hios_id) { nil }
-  let(:subscriber_start) { "20180301" }
-
-  subject do
-    LegacyEdiTransformations::TuftsSubscriberInfo.new(
-      subscriber_id,
-      hios_id,
-      capi,
-      subscriber_start
-    )
-  end
-
-  it "finds nothing" do
-    expect(subject.locate_policy_information).to eq nil
-  end
-end
-
-describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
-  - given a subscriber id
-  - given a carrier assigned policy id
-  - given a hios id
-  - given a blank subscriber start
-" do
-  let(:subscriber_id) { "A SUBSCRIBER ID" }
-  let(:capi) { "A CARRIER ASSIGNED POLICY ID" }
-  let(:hios_id) { "A HIOS ID" }
-  let(:subscriber_start) { nil }
-
-  subject do
-    LegacyEdiTransformations::TuftsSubscriberInfo.new(
-      subscriber_id,
-      hios_id,
-      capi,
-      subscriber_start
-    )
-  end
-
-  it "finds nothing" do
-    expect(subject.locate_policy_information).to eq nil
-  end
-end
-
-describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
-  - given a subscriber id
-  - given a blank carrier assigned policy id
+  - given a blank subscriber id
+  - given an exchange assigned policy id
   - given a hios id
   - given a subscriber start
+  - the policy does not exist
 " do
-
-  let(:subscriber_id) { "A SUBSCRIBER ID" }
-  let(:capi) { nil }
+  let(:subscriber_id) { nil }
+  let(:eapi) { "AN EXCHANGE ASSIGNED POLICY ID" }
   let(:hios_id) { "A HIOS ID" }
   let(:subscriber_start) { "20180301" }
 
@@ -91,86 +42,76 @@ describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
     LegacyEdiTransformations::TuftsSubscriberInfo.new(
       subscriber_id,
       hios_id,
-      capi,
+      eapi,
       subscriber_start
     )
   end
 
-  let(:plan) do
-    instance_double(
-      Plan,
-      :id => plan_id
+  it "finds nothing" do
+    expect(subject.locate_policy_information).to eq nil
+  end
+end
+
+describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
+  - given a subscriber id
+  - given an exchange assigned policy id
+  - given a hios id
+  - given a subscriber start
+  - the policy does exist
+" do
+
+  let(:subscriber_id) { "A SUBSCRIBER ID" }
+  let(:eapi) { nil }
+  let(:hios_id) { "A HIOS ID" }
+  let(:subscriber_start) { "20180301" }
+
+  subject do
+    LegacyEdiTransformations::TuftsSubscriberInfo.new(
+      subscriber_id,
+      hios_id,
+      eapi,
+      subscriber_start
     )
-  end
-
-  let(:plan_id) { "A PLAN ID" }
-
-  before :each do
-    allow(Plan).to receive(:where).with(
-      {:hios_plan_id => hios_id}
-    ).and_return(plan_results)
-  end
-
-  describe "which has no matching plans" do
-    let(:plan_results) { [] }
-
-    it "finds nothing" do
-      expect(subject.locate_policy_information).to eq nil
-    end
   end
 
   describe "finding a policy with no employer" do
-    let(:plan_results) { [plan] }
-
-    let(:policy_id) { "AN EG ID" }
-
-    let(:enrollee) do
-      instance_double(
-        Enrollee,
-        :m_id => subscriber_id,
-        :coverage_start => Date.new(2018, 3, 1),
-        :subscriber? => true
-      )
-    end
+    let(:eapi) { "AN EG ID" }
 
     let(:policy) do
       instance_double(
         Policy,
-        :eg_id => policy_id,
-        :employer => nil,
-        :enrollees => [enrollee]
+        :eg_id => eapi,
+        :employer => nil
       )
     end
 
     before :each do
       allow(Policy).to receive(:where).with(
         {
-          :plan_id => {"$in" => [plan_id]},
-          "enrollees.m_id" => subscriber_id
+          :eg_id => eapi
         }
       ).and_return([policy])
     end
 
     it "finds the policy id but no employer information" do
-      expect(subject.locate_policy_information).to eq [policy_id,nil,nil]
+      expect(subject.locate_policy_information).to eq [eapi,nil,nil]
     end
   end
 
   describe "finding a policy with an employer" do
-    let(:plan_results) { [plan] }
+    let(:eapi) { "AN EG ID" }
 
-    let(:policy_id) { "AN EG ID" }
-    let(:employer_name) { "AN EMPLOYER NAME" }
-    let(:employer_fein) { "AN EMPLOYER FEIN" }
-
-    let(:enrollee) do
+    let(:policy) do
       instance_double(
-        Enrollee,
-        :m_id => subscriber_id,
-        :coverage_start => Date.new(2018, 3, 1),
-        :subscriber? => true
+        Policy,
+        :eg_id => eapi,
+        :employer => employer
       )
     end
+
+    let(:eapi) { "AN EG ID" }
+    let(:employer_name) { "AN EMPLOYER NAME" }
+    let(:employer_fein) { "AN EMPLOYER FEIN" }
 
     let(:employer) do
       instance_double(
@@ -183,23 +124,21 @@ describe LegacyEdiTransformations::TuftsSubscriberInfo, "given:
     let(:policy) do
       instance_double(
         Policy,
-        :eg_id => policy_id,
-        :employer => employer,
-        :enrollees => [enrollee]
+        :eg_id => eapi,
+        :employer => employer
       )
     end
 
     before :each do
       allow(Policy).to receive(:where).with(
         {
-          :plan_id => {"$in" => [plan_id]},
-          "enrollees.m_id" => subscriber_id
+          :eg_id => eapi
         }
       ).and_return([policy])
     end
 
     it "finds the policy id and employer information" do
-      expect(subject.locate_policy_information).to eq [policy_id,employer_name,employer_fein]
+      expect(subject.locate_policy_information).to eq [eapi,employer_name,employer_fein]
     end
   end
 end

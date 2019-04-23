c793254aa840409e22b92d4562cec0276925ea8c
harsha280 <harsha.vskr@gmail.com>
2019-04-23 09:37:57 -0400

    Refs-40696  Modified rake

--- app/data_migrations/remove_issuer_ids_from_plan_year.rb
+++ app/data_migrations/remove_issuer_ids_from_plan_year.rb
@@ -9,13 +9,13 @@ class RemoveIssuerIdsFromPlanYear < MongoidMigrationTask
     employer = Employer.where(hbx_id: hbx_id).first
 
     if employer.blank?
-      puts "No employer was found with the given hbx_id: hbx_id } " unless Rails.env.test?
+      puts "No employer was found with the given hbx_id: #{hbx_id} " unless Rails.env.test?
     else
       plan_year = employer.plan_years.where(start_date: start_date).first
       if plan_year.present?
-        plan_year.issuer_ids.delete(issuer_id)
+        issuer = plan_year.issuers.delete_if{|issuer| issuer.id.to_s == issuer_id}
+        plan_year.issuer_ids = issuer.map(&:id)
         plan_year.save
-        puts plan_year.issuer_ids
         puts "Successfully updated issuer ids for plan_year with start_date:#{plan_year.start_date} and end_date:#{plan_year.end_date}" unless Rails.env.test?
       else
         puts "Plan_year is not found for the employer: #{hbx_id}" unless Rails.env.test?

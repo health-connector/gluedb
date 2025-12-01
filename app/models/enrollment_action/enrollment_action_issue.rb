4f2a6516a4aa77d00031e30d8742648ce1f72e4d
nks2109 <nikhilks219@gmail.com>
2025-12-01 17:45:37 -0500

    Add performance indexes

--- app/models/enrollment_action/enrollment_action_issue.rb
+++ app/models/enrollment_action/enrollment_action_issue.rb
@@ -16,6 +16,11 @@ module EnrollmentAction
 
     index({received_at: 1, batch_id: 1, batch_index: 1, error_message: 1, "headers.return_status" => 1})
     index({received_at: -1, batch_id: -1, batch_index: -1, "headers.return_status" => 1}, {name: "default_order_index"})
+    # Performance indexes for enrollment action batch processing and error tracking
+    index({hbx_enrollment_id: 1})
+    index({aasm_state: 1, received_at: -1})
+    index({"headers.return_status" => 1, received_at: -1})
+    index({batch_id: 1, aasm_state: 1})
 
     scope :default_order, ->{ where({"headers.return_status" => {"$ne" => "200"}}).desc(:received_at, :batch_id, :batch_index)  }
 
